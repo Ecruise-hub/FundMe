89a8a1a1ba9f5a59417baf49b493241770b03bad
Ecruise-hub <enocholaoluwa401@gmail.com>
2026-09-19 07:15:50 +0100

    Update FundMe.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -11,28 +11,28 @@ import {PriceConverter} from "./PriceConverter.sol";
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
 
     address[]public funders;
     mapping(address funders => uint256 amountFunded) public addressToAmountFunded;
     
-    address public owner;
+    address public immutable i_owner;
     constructor() {
-     owner = msg.sender; 
+     i_owner = msg.sender; 
     }
 
     modifier onlyowner() {
-        require(msg.sender == owner, "Not the owner");
+        require(msg.sender == i_owner, "Not the owner");
         _;
     }
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough eth"); // Requires the user to not send less than 5usd worth of eth
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough eth"); // Requires the user to not send less than 5usd worth of eth
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + (msg.value); 
     }  
 
     function withdraw() public onlyowner {
-        require(msg.sender == owner, "Must be the owner");
+        require(msg.sender == i_owner, "Must be the owner");
        for (uint256 funderIndex=0; funderIndex<funders.length; funderIndex++){
         address funder = funders[funderIndex];
         addressToAmountFunded[funder] = 0;
@@ -44,5 +44,13 @@ contract FundMe {
        (bool callsuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
        require(callsuccess, "Call failed");
     }  
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
 }
  
